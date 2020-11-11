2a8c993bfa9edda1d91f61de428c704b25a8dfd7
Philipp Hinz <phisn@outlook.de>
2020-11-11 02:00:33 +0100

    added library binary loading and environment helper

--- source/DynlecCompiler/Library.h
+++ source/DynlecCompiler/Library.h
@@ -6,6 +6,13 @@
 #include <sstream>
 #include <unordered_map>
 
+/*
+environement:
+- name [required]
+	Specifies the name used in import
+- binary_name [optional]
+	Set binary dll name. If not specified the name is used instead
+*/
 class Library
 {
 public:
@@ -25,9 +32,53 @@ public:
 		}
 	}
 
+	void loadBinary(std::vector<std::filesystem::path>& pathBinaryLibraries)
+	{
+		const std::string& name = getBinaryName();
+
+		bool binaryFound = false;
+		for (const std::filesystem::path& path : pathBinaryLibraries)
+			if (path.stem() == name)
+			{
+				setBinary(path);
+				return;
+			}
+
+		std::ostringstream oss;
+		oss << "Library binary not found \"";
+		oss << path << "\"";
+
+		throw std::exception(oss.str().c_str());
+	}
+
+	const std::string& getBinaryName()
+	{
+		auto name = environment.find("binary_name");
+
+		return name == environment.cend()
+			? environment["name"]
+			: name->second;
+	}
+
+	const std::string& getName()
+	{
+		return environment["name"];
+	}
+
 private:
+	std::vector<unsigned char> binary;
+
 	std::filesystem::path path;
 	std::unordered_map<std::string, std::string> environment;
 
 	void parse();
+
+	void setBinary(const std::filesystem::path& location)
+	{
+		binary.reserve(std::filesystem::file_size(location));
+
+		std::ifstream file(location);
+		std::istream_iterator<unsigned char> start(file), end;
+		binary.assign(start, end);
+	}
 };
