2356edb518addedc6e99397822152f8200015faf
Philipp Hinz <phisn@outlook.de>
2020-11-11 02:01:02 +0100

    added library binary loading and started source file parsing

--- source/DynlecCompiler/main.cpp
+++ source/DynlecCompiler/main.cpp
@@ -20,13 +20,19 @@ bool isSourceFile(std::filesystem::path& path)
 		|| path.extension() == ".h++";
 }
 
-void compile();
+void compile(std::string libraryPath, std::string sourcePath);
+
+void Prepare(std::string& sourcePath, std::string& libraryPath, std::vector<std::filesystem::path>& pathSources, std::vector<std::unique_ptr<Library>>& libraries);
+
+void LoadLibraryBinaries(std::string& libraryPath, std::vector<std::unique_ptr<Library>>& libraries);
+
+void FindLibraryBinaries(std::string& libraryPath, std::vector<std::filesystem::path>& pathBinaryLibraries);
 
 int main()
 {
 	try 
 	{
-		compile();
+		compile("dynleclib", ".");
 		std::cout << "Compilation succeeded" << std::endl;
 	}
 	catch (std::exception e)
@@ -37,34 +43,104 @@ int main()
 }
 
 void MapDirectory(
-	std::filesystem::recursive_directory_iterator& iterator,
+	std::string sourcePath,
 	std::vector<std::filesystem::path>& pathLibraries,
 	std::vector<std::filesystem::path>& pathSources);
 
-void compile()
+void compile(std::string libraryPath, std::string sourcePath)
 {
-	std::vector<std::filesystem::path> pathLibraries;
+	std::vector<std::unique_ptr<Library>> libraries;
 	std::vector<std::filesystem::path> pathSources;
 
-	std::filesystem::recursive_directory_iterator iterator("");
+	Prepare(sourcePath, libraryPath, pathSources, libraries);
 
-	MapDirectory(iterator, pathLibraries, pathSources);
+	std::regex pattern("^\\s*//:dynlec:(.*):(.*)");
+	std::smatch matches;
 
-	std::vector<std::unique_ptr<Library>> libraries;
+	for (std::filesystem::path& source : pathSources)
+	{
+		std::vector<std::string> lines;
+		std::ifstream file(source);
+
+		for (std::string line; std::getline(file, line);)
+			lines.push_back(std::move(line));
+
+		for (int i = 0; i < lines.size(); ++i)
+		{
+			if (std::regex_match(lines[i], matches, pattern))
+			{
+				if (matches[0] == "import")
+				{
+					auto library = std::find_if(libraries.begin(), libraries.end(),
+						[&matches](std::unique_ptr<Library> library)
+						{
+							return matches[1] == library->getName();
+						});
+
+
+
+					continue;
+				}
+
+				std::ostringstream oss;
+				oss << "failed to parse dynlec call \"";
+				oss << source << "\"";
+				oss << "at line (" << i << ")";
+
+				throw std::exception(oss.str().c_str());
+			}
+		}
+
+
+	}
+}
+
+//:dynlec:import:file
+
+void Prepare(std::string& sourcePath, std::string& libraryPath, std::vector<std::filesystem::path>& pathSources, std::vector<std::unique_ptr<Library>>& libraries)
+{
+	std::vector<std::filesystem::path> pathLibraries;
+
+	MapDirectory(sourcePath, pathLibraries, pathSources);
 
 	for (std::filesystem::path& path : pathLibraries)
 	{
 		libraries.push_back(std::make_unique<Library>(path));
 	}
 
+	LoadLibraryBinaries(libraryPath, libraries);
+}
 
+void LoadLibraryBinaries(std::string& libraryPath, std::vector<std::unique_ptr<Library>>& libraries)
+{
+	std::vector<std::filesystem::path> pathBinaryLibraries;
+
+	FindLibraryBinaries(libraryPath, pathBinaryLibraries);
+
+	for (std::unique_ptr<Library>& library : libraries)
+	{
+		library->loadBinary(pathBinaryLibraries);
+	}
+}
+
+void FindLibraryBinaries(std::string& libraryPath, std::vector<std::filesystem::path>& pathBinaryLibraries)
+{
+	std::filesystem::recursive_directory_iterator libraryIterator(libraryPath);
+
+	for (const std::filesystem::directory_entry& entry : libraryIterator)
+	{
+		if (entry.path().extension() == ".dll")
+			pathBinaryLibraries.push_back(entry.path());
+	}
 }
 
 void MapDirectory(
-	std::filesystem::recursive_directory_iterator& iterator, 
+	std::string sourcePath,
 	std::vector<std::filesystem::path>& pathLibraries, 
 	std::vector<std::filesystem::path>& pathSources)
 {
+	std::filesystem::recursive_directory_iterator iterator(sourcePath);
+
 	for (const std::filesystem::directory_entry& directory : iterator)
 	{
 		std::filesystem::path extension = directory.path().extension();
